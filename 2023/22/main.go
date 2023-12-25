a5e0b0b2b4eb5ddc83c0bcea2f58b63b2e2f5bfc
rogeryk <rogeryk@outlook.com>
2023-12-25 21:47:32 +0800

    add 22

---
+++ 2023/22/main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"cmp"
+	_ "embed"
+	"slices"
+	"strings"
+
+	"github.com/spf13/cast"
+)
+
+//go:embed input.txt
+var input string
+
+// func init() {
+// 	input = `1,0,1~1,2,1
+// 0,0,2~2,0,2
+// 0,2,3~2,2,3
+// 0,0,4~0,2,4
+// 2,0,5~2,2,5
+// 0,1,6~2,1,6
+// 1,1,8~1,1,9`
+// }
+
+func main() {
+	solve1()
+	solve2()
+}
+
+func solve1() {
+	ans := 0
+	bricks, supportTo, supportBy := getBricks()
+	for i := range bricks {
+		flag := true
+		for _, j := range supportTo[i] {
+			if len(supportBy[j]) < 2 {
+				flag = false
+			}
+		}
+		if flag {
+			ans++
+		}
+	}
+	println(ans)
+}
+
+func solve2() {
+	bricks, supportTo, supportBy := getBricks()
+	ans := 0
+	for i := range bricks {
+		q := []int{i}
+		falling := map[int]int{}
+		for len(q) != 0 {
+			v := q[0]
+			q = q[1:]
+			for _, j := range supportTo[v] {
+				falling[j]++
+				if len(supportBy[j]) <= falling[j] {
+					q = append(q, j)
+					ans++
+				}
+			}
+		}
+	}
+	println(ans)
+}
+
+func getBricks() (bricks [][]int, supportTo, supportBy map[int][]int) {
+	for _, line := range strings.Split(input, "\n") {
+		start, end, _ := strings.Cut(line, "~")
+		var startPos, endPos []int
+		for _, v := range strings.Split(start, ",") {
+			startPos = append(startPos, cast.ToInt(v))
+		}
+		for _, v := range strings.Split(end, ",") {
+			endPos = append(endPos, cast.ToInt(v))
+		}
+		if startPos[2] <= endPos[2] {
+			bricks = append(bricks, append(startPos, endPos...))
+		} else {
+			bricks = append(bricks, append(endPos, startPos...))
+		}
+	}
+	slices.SortFunc(bricks, func(a, b []int) int {
+		return cmp.Compare(a[2], b[2])
+	})
+	for i, a := range bricks {
+		minZ := 1
+		for _, b := range bricks[:i] {
+			if overlap(a, b) {
+				minZ = max(minZ, b[5]+1)
+			}
+		}
+		fall := a[2] - minZ
+		a[2] -= fall
+		a[5] -= fall
+	}
+	slices.SortFunc(bricks, func(a, b []int) int {
+		return cmp.Compare(a[2], b[2])
+	})
+
+	supportTo = map[int][]int{}
+	supportBy = map[int][]int{}
+	for i := 0; i < len(bricks); i++ {
+		for j := i + 1; j < len(bricks); j++ {
+			if support(bricks[i], bricks[j]) {
+				supportTo[i] = append(supportTo[i], j)
+				supportBy[j] = append(supportBy[j], i)
+			}
+		}
+	}
+
+	return
+}
+
+func overlap(a, b []int) bool {
+	return max(a[0], b[0]) <= min(a[3], b[3]) && max(a[1], b[1]) <= min(a[4], b[4])
+}
+
+func support(a, b []int) bool {
+	return overlap(a, b) && a[5]+1 == b[2]
+}
