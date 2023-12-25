4cc0f8f1a8042088023cdca55da31dffd69de15b
rogeryk <rogeryk@outlook.com>
2023-12-25 21:49:15 +0800

    fmt

--- 2023/22/main.go
+++ 2023/22/main.go
@@ -12,16 +12,6 @@ import (
 //go:embed input.txt
 var input string
 
-// func init() {
-// 	input = `1,0,1~1,2,1
-// 0,0,2~2,0,2
-// 0,2,3~2,2,3
-// 0,0,4~0,2,4
-// 2,0,5~2,2,5
-// 0,1,6~2,1,6
-// 1,1,8~1,1,9`
-// }
-
 func main() {
 	solve1()
 	solve2()
